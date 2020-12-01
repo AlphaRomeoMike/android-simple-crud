e98cfcf50691f3c308786627f5095f3a570fd6e5
Abdul Rafay Modi <subjectalpha32@gmail.com>
2020-12-01 11:24:15 +0500

    Insertion and fetching data has been completed

---
+++ app/src/main/java/com/example/crudapplication/DatabaseHelper.java
@@ -0,0 +1,52 @@
+package com.example.crudapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "student.db";
+    public static final String TABLE_NAME = "tbl_student";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "SURNAME";
+    public static final String COL_4 = "MARKS";
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, MARKS TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name, String surname, String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return res;
+    }
+}
---
+++ app/src/main/java/com/example/crudapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.crudapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextName, EditTextSurname, editTextMarks;
+    Button buttonAdd, ViewAll, UpdateButton;
+    DatabaseHelper myDB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+//        System.out.println(1);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Buttons casted
+        editTextName = findViewById(R.id.EditTextFirstName);
+        EditTextSurname = findViewById(R.id.EditTextLastName);
+        editTextMarks = findViewById(R.id.EditTextMarks);
+        buttonAdd = findViewById(R.id.fab);
+        ViewAll = findViewById(R.id.ViewButton);
+        UpdateButton = findViewById(R.id.UpdateButton);
+        myDB = new DatabaseHelper(MainActivity.this);
+        //Later
+        AddData();
+        viewAll();
+
+    }
+
+    public void AddData() {
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isinserted = myDB.insertData(editTextName.getText().toString(),
+                        EditTextSurname.getText().toString(),
+                        editTextMarks.getText().toString());
+                if(isinserted = true) {
+                    Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Data was not inserted", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    public void viewAll() {
+        ViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor res = myDB.getAllData();
+                if(res.getCount() == 0) {
+                    showMessage("No data", "Nothing in the database");
+                } else {
+                    StringBuffer buffer = new StringBuffer();
+                    while (res.moveToNext()) {
+                        buffer.append("ID: " + res.getString(0) + "\n");
+                        buffer.append("First Name: " + res.getString(1) + "\n");
+                        buffer.append("Sur name: " + res.getString(2) + "\n\n");
+                    }
+                    showMessage("Data", buffer.toString());
+                }
+            }
+        });
+    }
+    public void showMessage(String title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+}
